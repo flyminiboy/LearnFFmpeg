322917f1503934aec42d206ada4026b896d7e7af
fly <qifei1992@sina.cn>
2022-01-08 22:56:28 +0800

    添加权限库

--- app/src/main/java/com/gpf/ffmpeg/MainActivity.kt
+++ app/src/main/java/com/gpf/ffmpeg/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.gpf.ffmpeg
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.gpf.ffmpeg.databinding.ActivityMainBinding
+import com.permissionx.guolindev.PermissionX
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +24,31 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         binding.sampleText.text = FFmpeg.displayFFmpegInfo()
+
+        val mVideoPath =
+            Environment.getExternalStorageDirectory().absolutePath + "/gpf/a.mp4"
+
+        // 权限判断
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // 开始解码
+            FFmpeg.decodeVideo(mVideoPath)
+        } else {
+            PermissionX.init(this)
+                .permissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .request { allGranted, grantedList, deniedList ->
+                    if (allGranted) {
+                        // 开始解码
+                        FFmpeg.decodeVideo(mVideoPath)
+                    } else {
+                        Toast.makeText(this, "These permissions are denied: $deniedList", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+
     }
 
 }
